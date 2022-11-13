eed54cd4220c7667bcb53874d1fa8d4d79112095
hauwa.habib <108408200+hauwahabib@users.noreply.github.com>
2022-11-13 22:36:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+print("Greetings in different languages!")
+greetingslist = ["Hello", "Bonjour", "Hola", "Salve", "Konnichiwa","Guten Tag","Halo","Hey","Hei","Shalom","Merhaba","Namaste","Selamat siang", "Yassas","Shikamoo","As-salamu Alaikum","Anyoung haseyo","Hallo"]
+lists = random.randint(0,17)
+print(greetingslist[lists])
